c370e67214783b7d4092904a69422ed21c1a2783
EndCar808 <endacar808@gmail.com>
2023-02-21 14:45:01 +0000

    POST: Added ddatatypes header file, changed to global structs

---
+++ PostProcessing/src/data_types.h
@@ -0,0 +1,137 @@
+/**
+* @file data_types.h 
+* @author Enda Carroll
+* @date Feb 2023
+* @brief file containing the main data types and global variables
+*/
+// ---------------------------------------------------------------------
+//  Standard Libraries and Headers
+// ---------------------------------------------------------------------
+#ifndef __DATA_TYPES
+#ifndef __HDF5_HDR
+#include <hdf5.h>
+#include <hdf5_hl.h>
+#define __HDF5_HDR
+#endif
+#ifndef __FFTW3
+#include <fftw3.h>
+#define __FFTW3
+#endif
+#ifndef __OPENMP
+#include <omp.h>
+#define __OPENMP
+#endif
+#include <gsl/gsl_histogram.h> 
+#include <gsl/gsl_statistics_double.h>
+#include <gsl/gsl_rstat.h>
+#include <gsl/gsl_math.h>
+// ---------------------------------------------------------------------
+//  Compile Time Macros and Definitions
+// ---------------------------------------------------------------------
+#define checkError(x) ({int __val = (x); __val == -1 ? \
+	({fprintf(stderr, "ERROR ("__FILE__":%d) -- %s\n", __LINE__, strerror(errno)); \
+	exit(-1);-1;}) : __val; })
+
+// For coloured printing to screen
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define RESET   "\x1b[0m"
+// ---------------------------------------------------------------------
+//  Global Variables
+// ---------------------------------------------------------------------
+// These definitions define some of the solver parameters.
+#define SYS_DIM 3 				// The system dimension i.e., 3D
+// Statistics definitions
+#define N_BINS 1000				// The number of histogram bins to use
+#define NUM_INCR 2              // The number of increment length scales
+#define INCR_TYPES 2 			// The number of increment directions i.e., longitudinal and transverse
+#define BIN_LIM 40              // The limit of the bins for the velocity increments
+#define NUM_POW 6               // The number of powers for the str function
+// ---------------------------------------------------------------------
+//  Global Struct Definitions
+// ---------------------------------------------------------------------
+// System variables struct
+typedef struct system_vars_struct {
+	char u0[64];						// String to indicate the initial condition to use
+	char forcing[64];					// String to indicate what type of forcing is selected
+	long int N[SYS_DIM];				// Array holding the no. of collocation pts in each dim
+	fftw_plan fftw_3d_dft_r2c;			// FFTW plan to perform transform from Real to Fourier
+	fftw_plan fftw_3d_dft_c2r;			// FFTW plan to perform transform from Fourier to Real
+	fftw_plan fftw_3d_dft_batch_r2c;	// FFTW plan to perform a batch transform from Real to Fourier
+	long int num_snaps;					// Number of snapshots in the input data file
+	long int kmax; 						// The largest dealiased wavenumber
+	double t0;							// Intial time
+	double T;							// Final time
+	double t;							// Time variable
+	double dt;							// Timestep
+	double dx;							// Collocation point spaceing in the x direction
+	double dy;							// Collocation point spacing in the y direction	
+	double dz;							// Collocation point spacing in the z direction
+	int num_threads;					// The number of OMP threads to use
+	int thread_id;						// The ID of the OMP threads
+	int Max_Incr;
+} system_vars_struct;
+
+// Runtime data struct
+typedef struct runtime_data_struct {
+	double* x[SYS_DIM];      // Array to hold collocation pts
+	int* k[SYS_DIM];		 // Array to hold wavenumbers
+	fftw_complex* w_hat;     // Fourier space vorticity
+	fftw_complex* tmp_w_hat; // Temporary Fourier space vorticity
+	fftw_complex* u_hat;     // Fourier space velocity
+	fftw_complex* tmp_u_hat; // Fourier space velocity
+	double* tmp_u; 			 // Temporary array to read & write in velocities
+	double* w;				 // Real space vorticity
+	double* u;				 // Real space velocity
+	double* time;			 // Array to hold the simulation times
+} runtime_data_struct;
+
+// Post processing stats data struct
+typedef struct stats_data_struct {
+	gsl_rstat_workspace* r_stat_grad_u[SYS_DIM + 1];		  		// Workplace for the running stats for the gradients of velocity (both for each direction and combined)
+	gsl_rstat_workspace* r_stat_grad_w[SYS_DIM + 1];		  		// Workplace for the running stats for the gradients of vorticity (both for each direction and combined)
+	gsl_rstat_workspace* r_stat_w;									// Workplace for the running stats for the velocity (both for each direction and combined)
+	gsl_rstat_workspace* r_stat_u[SYS_DIM + 1];						// Workplace for the running stats for the vorticity (both for each direction and combined)
+	gsl_rstat_workspace* r_stat_vel_incr[INCR_TYPES][NUM_INCR];		// Workplace for the running stats for the velocity increments
+	gsl_rstat_workspace* r_stat_vort_incr[INCR_TYPES][NUM_INCR];	// Workplace for the running stats for the vorticity increments
+	gsl_histogram* w_hist;		 									// Histogram struct for the vorticity distribution
+	gsl_histogram* u_hist;		  									// Histrogam struct for the velocity distribution
+	gsl_histogram* vel_grad[SYS_DIM + 1];		 					// Array to hold the PDFs of the longitudinal and transverse velocity gradients 
+	gsl_histogram* vort_grad[SYS_DIM + 1];		 					// Array to hold the PDFs of the longitudinal and transverse vorticity gradients 
+	gsl_histogram* vel_incr[INCR_TYPES][NUM_INCR]; 					// Array to hold the PDFs of the longitudinal and transverse velocity increments for each increment
+	gsl_histogram* w_incr[INCR_TYPES][NUM_INCR]; 					// Array to hold the PDFs of the longitudinal and transverse vorticity increments for each increment
+} stats_data_struct;
+
+// HDF5 file info struct
+typedef struct HDF_file_info_struct {
+	char input_file_name[512];		// Array holding input file name
+	char output_file_name[512];     // Output file name array
+	char output_dir[512];			// Output directory
+	char input_dir[512];			// Input directory
+	char output_tag[64]; 			// Tag to be added to the output directory
+	hid_t output_file_handle;		// File handle for the output file 
+	hid_t input_file_handle;		// File handle for the input file 
+	hid_t COMPLEX_DTYPE;			// Complex datatype handle
+} HDF_file_info_struct;
+
+// Complex datatype struct for HDF5
+typedef struct complex_type_tmp {
+	double re;   		// real part 
+	double im;   		// imaginary part 
+} complex_type_tmp;
+
+// Declare the global variable pointers across all files
+extern system_vars_struct *sys_vars; 		    // Global pointer to system parameters struct
+extern runtime_data_struct *run_data; 			// Global pointer to system runtime variables struct 
+extern HDF_file_info_struct *file_info; 		// Global pointer to system forcing variables struct 
+extern stats_data_struct *stats_data;           // Globale pointer to the statistics struct
+
+#define __DATA_TYPES
+#endif
+// ---------------------------------------------------------------------
+//  End of File
+// ---------------------------------------------------------------------
--- PostProcessing/src/main.c
+++ PostProcessing/src/main.c
@@ -9,39 +9,88 @@
 // ---------------------------------------------------------------------
 #include <stdlib.h>
 #include <stdio.h>
-#include <complex.h>
 #include <math.h>
 #include <time.h>
-#include <fftw3.h>
-#include <omp.h>
 // ---------------------------------------------------------------------
 //  User Libraries and Headers
 // ---------------------------------------------------------------------
-// #include "data_types.h"
+#include "data_types.h"
+#include "hdf5_func.h"
 // #include "utils.h"
-// #include "post_proc.h"
+// #include "str_func.h"
 // ---------------------------------------------------------------------
 //  Global Variables Declarations
 // ---------------------------------------------------------------------
-#define NUM_POW 6
-#define NUM_INCR 2
-#define SYS_DIM 3
+// Define the global points that will be pointed to the global structs
+system_vars_struct*       sys_vars;
+runtime_data_struct*      run_data;
+HDF_file_info_struct*    file_info;
+stats_data_struct*      stats_data;
 // ---------------------------------------------------------------------
 //  Main function
 // ---------------------------------------------------------------------
 int main(int argc, char** argv) {
 
+	// --------------------------------
+	//  Create Global Stucts
+	// --------------------------------
+	// Create instances of global variables structs
+	runtime_data_struct runtime_data;
+	system_vars_struct   system_vars;
+	HDF_file_info_struct   HDF_file_info;
+	stats_data_struct statistics_data;
+
+	// Point the global pointers to these structs
+	run_data   = &runtime_data;
+	sys_vars   = &system_vars;
+	file_info  = &HDF_file_info;
+	stats_data = &statistics_data;
+
 	printf("\n\ngcc version: %d.%d.%d\n\n", __GNUC__,__GNUC_MINOR__,__GNUC_PATCHLEVEL__);
 
 	// Initialize variables
-	int Nx = atoi(argv[1]);
-	int Ny = Nx;
-	int Nz = Ny;
+	sys_vars->N[0] = atoi(argv[1]);
+	int Nx = sys_vars->N[0];
+	sys_vars->N[1] = Nx;
+	sys_vars->N[2] = Nx;
+	int Ny = sys_vars->N[1];
+	int Nz = sys_vars->N[2];
+	sys_vars->Max_Incr = Nx / 2;
+	sys_vars->num_threads = atoi(argv[2]);
+	int tmp1, tmp2, indx;
+
+	double Lx = 2.0 * M_PI;
+	double Ly = 2.0 * M_PI;
+	double Lz = 2.0 * M_PI;
+	sys_vars->dx = Lx / Nx;
+	sys_vars->dy = Ly / Ny;
+	sys_vars->dz = Lz / Nz;
+
+
+	// --------------------------------
+	//  Create Complex Datatype
+	// --------------------------------
+	// Create compound datatype for the complex datasets
+	file_info->COMPLEX_DTYPE = CreateComplexDatatype();
+
+
+	
+	printf("Nx: %d Lx: %lf dx: %lf\nNy: %d Ly: %lf dy: %lf\nNz: %d Lz: %lf dz: %lf\n\n", Nx, Lx, sys_vars->dx, Ny, Ly, sys_vars->dy, Nz, Lz, sys_vars->dz);
+
+	
+	return 0;
+}
+
+
+void str_func_test(int Nx, int Ny, int Nz, int num_thread) {
+
 	int Max_Incr = Nx / 2;
-	int num_thread = atoi(argv[2]);
 	double increment;
 	int tmp1, tmp2, indx;
-	int num_pow = NUM_POW;
+
+
+	omp_set_num_threads(num_thread);
+	int grain_size = NUM_POW * Max_Incr / num_thread;
 
 	double Lx = 1.0;
 	double Ly = 1.0;
@@ -50,10 +99,6 @@ int main(int argc, char** argv) {
 	double dy = Ly / Ny;
 	double dz = Lz / Nz;
 
-	omp_set_num_threads(num_thread);
-	int grain_size = NUM_POW * Max_Incr / num_thread;
-	printf("\n\nN: %d\t Max r: %d\t Num_pow: %d\tThreads: %d Grainsize: %d\n", Nx, Max_Incr, num_pow, num_thread, grain_size);
-	printf("Nx: %d Lx: %lf dx: %lf\nNy: %d Ly: %lf dy: %lf\nNz: %d Lz: %lf dz: %lf\n\n", Nx, Lx, dx, Ny, Ly, dy, Nz, Lz, dz);
 
 	// Allocate test memory
 	double* data = (double* )fftw_malloc(sizeof(double) * Nx * Ny * Nz * SYS_DIM);
@@ -73,6 +118,7 @@ int main(int argc, char** argv) {
 			}
 		}
 	}
+	
 	for (int p = 1; p <= NUM_POW; ++p) {
 		str_func_ser[p - 1] = (double* )fftw_malloc(sizeof(double) * Max_Incr);
 		str_func_par[p - 1] = (double* )fftw_malloc(sizeof(double) * Max_Incr);
@@ -82,15 +128,21 @@ int main(int argc, char** argv) {
 		}
 	}
 
+	
+
+
+	printf("\n\nN: %d\t Max r: %d\t Num_pow: %d\tThreads: %d Grainsize: %d\n", Nx, Max_Incr, NUM_POW, num_thread, grain_size);
+
+
 	// Get parllel time
 	double start = omp_get_wtime();
 	
 	// // Loop over powers
-	#pragma omp parallel num_threads(num_thread) shared(data, str_func_par) private(tmp1, tmp2, indx)
-	{
-		#pragma omp single 
-		{
-			#pragma omp taskloop reduction(+:increment) grainsize(grain_size) collapse(2)
+	// #pragma omp parallel num_threads(num_thread) shared(data, str_func_par) private(tmp1, tmp2, indx)
+	// {
+	// 	#pragma omp single 
+	// 	{
+	// 		#pragma omp taskloop reduction(+:increment) grainsize(grain_size) collapse(2)
 			for (int p = 1; p <= NUM_POW; ++p) {
 				// Loop over increments
 				for (int r = 1; r <= Max_Incr; ++r) {
@@ -118,8 +170,8 @@ int main(int argc, char** argv) {
 					str_func_par[p - 1][r - 1] = increment; 
 				}
 			}
-		} 
-	}
+	// 	} 
+	// }
 
 	// Get parallel time
 	double par_time = omp_get_wtime() - start;
@@ -134,11 +186,10 @@ int main(int argc, char** argv) {
 	}
 	printf("str_func[%d][%d]: %1.16lf\n", 0, 0, err);
 
+
 	for (int p = 0; p < NUM_POW; ++p) {
 		fftw_free(str_func_ser[p]);
 		fftw_free(str_func_par[p]);
 	}
 	fftw_free(data);
-
-	return 0;
 }
